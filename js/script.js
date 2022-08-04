fc0d7f137aeec66fe06fbfb46ad50631624837b8
lksamy <lknxtgen@gmail.com>
2022-08-04 20:10:31 +0530

    varan dropdown design fixed

---
+++ js/script.js
@@ -0,0 +1,11 @@
+  $(".toggle-password").click(function() {
+    $(this).toggleClass("fa-eye fa-eye-slash");
+    input = $(this).parent().find("input");
+    if (input.attr("type") == "password") {
+        input.attr("type", "text");
+    } else {
+        input.attr("type", "password");
+    }
+  });
+
+
