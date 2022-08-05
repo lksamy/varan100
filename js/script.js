197edbf3f6fb1bd362b8b7297a8088f0b867e871
lksamy <lknxtgen@gmail.com>
2022-08-05 20:01:13 +0530

    side fixed content fixed check dashboard

--- js/script.js
+++ js/script.js
@@ -8,4 +8,28 @@
     }
   });
 
+  $(document).ready(function () {
+    var $window = $(window);
+    var $sidebar = $(".sideSticky");
+    var $sidebarHeight = $sidebar.innerHeight();
+    var $footerOffsetTop = $(".footer").offset().top;
+    var $sidebarOffset = $sidebar.offset();
+  
+    $window.scroll(function () {
+      if ($window.scrollTop() > $sidebarOffset.top) {
+        $sidebar.addClass("fixed");
+      } else {
+        $sidebar.removeClass("fixed");
+      }
+      if ($window.scrollTop() + $sidebarHeight > $footerOffsetTop) {
+        $sidebar.css({
+          top: -($window.scrollTop() + $sidebarHeight - $footerOffsetTop)
+        });
+      } else {
+        $sidebar.css({ top: "0" });
+      }
+    });
+  });
+  
+  
 
