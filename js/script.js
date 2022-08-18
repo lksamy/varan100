5f3c2c3ef495af325cc9b5a2ff331cfa151be763
lksamy <lknxtgen@gmail.com>
2022-08-18 21:37:43 +0530

    varan dropdown design fixed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
- 
+$(function(){
   
-  
+  $('li.dropdown > a').on('click',function(event){
+    
+    event.preventDefault()
+    
+    $(this).parent().find('ul').first().toggle(300);
+    
+    $(this).parent().siblings().find('ul').hide(200);
+    
+    //Hide menu when clicked outside
+    $(this).parent().find('ul').mouseleave(function(){  
+      var thisUI = $(this);
+      $('html').click(function(){
+        thisUI.hide();
+        $('html').unbind('click');
+      });
+    });
+    
+    
+  });
+});
+
+
   $(".toggle-password").click(function() {
     $(this).toggleClass("fa-eye fa-eye-slash");
     input = $(this).parent().find("input");
