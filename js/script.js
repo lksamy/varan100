2484ec991f20426f9b39d87c7028014c86728eb1
lksamy <lknxtgen@gmail.com>
2022-08-22 20:08:33 +0530

    varan

--- js/script.js
+++ js/script.js
@@ -59,5 +59,14 @@ $(function(){
   });
   
   
-      
-
+    
+  
+    $(".js-select2").select2();
+  
+    
+    $(".js-select2-multi").select2();
+    
+    $(".large").select2({
+      dropdownCssClass: "big-drop",
+    });
+    
