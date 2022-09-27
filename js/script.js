9f174f878e7bae82c1c6b99f44612316ac1b1582
lksamy <lknxtgen@gmail.com>
2022-09-27 19:24:36 +0530

    varan dropdown design fixed

--- js/script.js
+++ js/script.js
@@ -59,14 +59,3 @@ $(function(){
   });
   
   
-    
-  
-    $(".js-select2").select2();
-  
-    
-    $(".js-select2-multi").select2();
-    
-    $(".large").select2({
-      dropdownCssClass: "big-drop",
-    });
-    
