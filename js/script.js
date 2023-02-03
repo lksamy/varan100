3aa94c92485e838011f6773f0f450e5c35371b5c
lksamy <lknxtgen@gmail.com>
2023-02-03 19:21:35 +0530

    varan mobile

--- js/script.js
+++ js/script.js
@@ -32,30 +32,3 @@ $(function(){
     }
   });
 
-  $(document).ready(function () {
-    var $window = $(window);
-    var $sidebar = $(".sideSticky");
-    var $sidebarHeight = $sidebar.innerHeight();
-    var $footerOffsetTop = $(".footer").offset().top;
-    var $sidebarOffset = $sidebar.offset();
-  
-    $window.scroll(function () {
-      if ($window.scrollTop() > $sidebarOffset.top) {
-        $sidebar.addClass("fixed");
-      } else {
-        $sidebar.removeClass("fixed");
-      }
-      if ($window.scrollTop() + $sidebarHeight > $footerOffsetTop) {
-        $sidebar.css({
-          top: -($window.scrollTop() + $sidebarHeight - $footerOffsetTop)
-        });
-      } else {
-        $sidebar.css({ top: "0" });
-      }
-    });
-
-
-    
-  });
-  
-  
