4a8c99b2a4fb07d6b72fe90fd52e46616e1fa18b
lksamy <lknxtgen@gmail.com>
2023-03-02 16:46:47 +0530

    varan100

--- js/script.js
+++ js/script.js
@@ -32,3 +32,30 @@ $(function(){
     }
   });
 
+  $(document).ready(function(){
+    $('.my-slider').slick({
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      arrows: true,
+      dots: true,
+      speed: 300,
+      infinite: true,
+      autoplaySpeed: 5000,
+      autoplay: true,
+      responsive: [
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+      }
+    }
+  ]
+    });
+  });
+
